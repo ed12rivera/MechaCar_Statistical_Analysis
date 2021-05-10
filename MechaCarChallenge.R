97fdf3335cc8f9642396b0d78f0730ea8e47b25e
Edward Rivera <72109265+ed12rivera@users.noreply.github.com>
2021-05-10 01:30:37 -0700

    Update R script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,24 +2,25 @@
 library(dplyr)
 library(tidyverse)
 
+# Linear regression to predict mpg
 # Read the csv file
 car_data <- read.csv("Data/MechaCar_mpg.csv")
 
 # Generate multiple linear regression model
 lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=car_data)
 
-summary(lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_data))
+summary(lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=car_data))
 
 # Suspension coil analysis
 suspension_data <- read.csv("Data/Suspension_Coil.csv")
 
 # Create summary DataFrames
-total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
-lot_summary <- suspension_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI) , Variance = var(PSI) , SD = sd(PSI),.groups = 'keep' )
+total_summary <- suspension_data %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI) , Variance = var(PSI) , SD = sd(PSI),.groups = 'keep' )
 
 # T-tests on suspension coils
-t.test(suspension_coil_df$PSI, mu=1500)
+t.test(suspension_data$PSI, mu=1500)
 
-t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
-t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
-t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
+t.test(subset(suspension_data,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_data,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_data,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
